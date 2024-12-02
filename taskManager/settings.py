d4fe88522fe1fc811dbd1352bf76c9052a655133
William Cho <william.cho@nsuslab.com>
2024-12-02 21:27:12 +0900

    modified settings

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -85,9 +85,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/html/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static")
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
